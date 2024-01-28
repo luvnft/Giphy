ed07f135d5426df3cfec4daca9d3f5c8009b97dd
luvnft <140175751+luvnft@users.noreply.github.com>
2024-01-28 02:27:23 -0500

    Update TrendingGifs.vue

--- src/components/TrendingGifs.vue
+++ src/components/TrendingGifs.vue
@@ -1,10 +1,8 @@
 <template>
-  <div>
-    <h1 class="text-4xl font-bold mb-4">Trending GIFs</h1>
-    <div
-      class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"
-    >
-      <div v-for="gif in gifs" :key="gif.id" @click="expandGif(gif)">
+  <div class="bg-black text-white">
+    <h1 class="text-4xl font-bold mb-4">🅰️GIF</h1>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      <div v-for="gif in gifs" :key="gif.id" @click="expandGif(gif)" class="border-white border">
         <img
           :src="gif.images.fixed_height.url"
           :alt="gif.title"
@@ -12,10 +10,19 @@
         />
       </div>
     </div>
+    <div class="mt-4">
+      <p>Trending Giphy GIFs for your Arvrtising campaign.</p>
+    </div>
     <button @click="expandGif(selectedGif)">Expand GIF</button>
 
     <!-- Gif Modal -->
     <GifModal v-if="isModalOpen" :gif="selectedGif" @close="closeModal" />
+    
+    <!-- Footer Section -->
+    <div class="mt-8 flex justify-between">
+      <div>Powered by <a href="https://arvrtise.com" class="text-white underline">🅰️rvrtise</a></div>
+      <div><a href="https://ateam.arvrtise.com" class="text-white underline">Subscribe to 🅰️-Team PPV Discord</a></div>
+    </div>
   </div>
 </template>
 
